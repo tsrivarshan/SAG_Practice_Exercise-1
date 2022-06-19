6b9a55cbb4386ffffa0227303a80592e413f1d24
tsrivarshan <70463437+tsrivarshan@users.noreply.github.com>
2022-06-19 13:08:32 +0530

    Update child.sh

--- child.sh
+++ child.sh
@@ -2,12 +2,20 @@
 #####################################################################################################
 #  Author : Srivarshan T
 #  Date created : 16-06-2022
-#  Description : Printing the numbers which is odd within the given limit
+#  Description : Printing the numbers which is prime within the given limit
 ######################################################################################################
 
 for((i=0;i<$1;i++))
 do
-if ((i%2!=0))
+c=1
+for((j=2;j<=i;j++))
+do
+if ((i%j == 0))
+then
+((c=c+1))
+fi
+done
+if ((c == 2))
 then echo $i
 fi
 done
